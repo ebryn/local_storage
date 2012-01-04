fce9e82c2217b26b08c4c9e87b06e1fc0389565c
Erik Bryn <erik.bryn@gmail.com>
2012-01-03 21:53:08 -0800

    Additional project files

---
+++ generators/license.js
@@ -0,0 +1,6 @@
+// ==========================================================================
+// Project:   LocalStorage - A Local Storage Framework for SproutCore
+// Copyright: ©2010 Strobe Inc., Peter Wagenet, and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
